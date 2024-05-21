3e13ffae3ba4c331cee0e09ce818e0610efaf55a
tree <2807823091@qq.com>
2024-05-21 14:13:43 +0800

    Update your_player.rb

--- your_player.rb
+++ your_player.rb
@@ -1,15 +1,26 @@
-require './base_player.rb'
+require_relative 'base_player'
 
 class YourPlayer < BasePlayer
-  def next_point(time:)
-    # Implement your strategy here.
-    {
-      row: 0,
-      col: 0
-    }
+  def initialize
+    super
+    @visited = {}
+    @current_position = {row: 0, col: 0}
   end
 
-  def grid
-    game.grid
+  def next_point(grid)
+    @visited[@current_position] = true
+    
+    neighbors = grid.edges[@current_position]
+
+    next_position = neighbors.keys.find { |point| !@visited[point] }
+    
+    unless next_position
+      unvisited = grid.edges.keys - @visited.keys
+      next_position = unvisited.sample unless unvisited.empty?
+    end
+
+    @current_position = next_position if next_position
+
+    next_position || @current_position
   end
 end
